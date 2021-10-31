d02d15ac43d71c941698a39bb55e0e5a7855e6d2
Bounama Ly <bounama.ly@gmail.com>
2021-10-31 09:47:07 +0100

    Initial release of the Parking Ticket System
    TODO#1 :  Fix the code to make the unit tests pass,
    TODO#2 : Fix integration tests with "TODO" comments,
    STORY#1 : Free 30-min parking,
    STORY#2 : 5%-discount for recurring users

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,55 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	private boolean isRecurring;
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
+		// getHours is deprecated.instead use the constructor Time stamp(long millis)
+		long inHour = ticket.getInTime().getTime();
+		long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+		// For the duration I use the double type to have a real number.
+		double duration = outHour - inHour;
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		// STORY#1 : Free 30-min parking
+		if (duration > 30 * 60 * 1000) {
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration / (60 * 60 * 1000) * Fare.CAR_RATE_PER_HOUR);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration / (60 * 60 * 1000) * Fare.BIKE_RATE_PER_HOUR);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
+		} else {
+			ticket.setPrice(0);
+		}
+
+		// STORY#2 : 5%-discount for recurring users
+		if (isRecurring) {
+			ticket.setPrice(ticket.getPrice() * 0.95);
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration / (60 * 60 * 1000) * Fare.CAR_RATE_PER_HOUR * 0.95);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration / (60 * 60 * 1000) * Fare.BIKE_RATE_PER_HOUR * 0.95);
+				break;
+			}
+			default:
+//				break;
+			}
+		} else {
+			ticket.setPrice(ticket.getPrice());
+		}
+
+	}
 }
